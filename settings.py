53f4c7c682e19e11a19d291c7585dfa1d38fa7df
chapkovski <chapkovski@gmail.com>
2020-06-21 13:36:17 -0400

    adding bots

--- settings.py
+++ settings.py
@@ -7,7 +7,8 @@ import os
 # e.g. self.session.config['participation_fee']
 
 SESSION_CONFIG_DEFAULTS = dict(
-    real_world_currency_per_point=1.00, participation_fee=0.00, doc=""
+    real_world_currency_per_point=1.00, participation_fee=0.00, doc="",
+    use_browser_bots=False
 )
 
 SESSION_CONFIGS = [
